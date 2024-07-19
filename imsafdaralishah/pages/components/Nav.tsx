d97d7eea88b36ab9caf8e2596603276b9af8aa0f
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-07-19 08:40:39 +0500

    gitUpdate

---
+++ imsafdaralishah/pages/components/Nav.tsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+    return (
+        <header>
+            <nav className="flex flex-row m-3 gap-5">
+                <button className="p-2 pl-4 pr-5 font-semibold text-[#1A4D2E] rounded-full bg-[#F5EFE6]">imSafdarAliShah</button>
+                <button className="p-2 pl-4 pr-5  text-white border rounded-full">Frontend Developer</button>
+            </nav>
+        </header>
+    )
+}
